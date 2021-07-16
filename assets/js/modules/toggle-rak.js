2dbf521c2b5f48b5376b05ce77b2fe015fd265c2
Dio Ilham <apajabole1@gmail.com>
2021-07-16 14:28:09 +0700

    #2: Branch 2 update more

---
+++ assets/js/modules/toggle-rak.js
@@ -0,0 +1,66 @@
+// initialize const element
+// button toggle menu left (default & large breakpoint)
+const leftContentBtnEl = document.getElementById('left-content-btn');
+const leftContentBtnLgEl = document.getElementById('left-content-btn-lg');
+const leftContentEl = document.getElementById('left-content');
+
+// button toggle menu right (default & large breakpoint)
+const rightContentBtnEl = document.getElementById('right-content-btn');
+const rightContentBtnLgEl = document.getElementById('right-content-btn-lg');
+const rightContentEl = document.getElementById('right-content');
+
+// get all Rak btn el and get all Rak content el
+const allToggleBtn = document.querySelectorAll('[data-toggle-for]');
+const allToggleContent = document.querySelectorAll('[data-toggle-from]');
+
+/**
+ * 
+ * Set specific Rak by button el and content el to be not active.
+ * 
+ * @param {obj} btnEl Button to be set not active
+ * @param {obj} contentEl Element to be set not active
+ */
+const setRakNotActive = (btnEl, contentEl) => {
+  btnEl.classList.remove('toggle__btn__active')
+  contentEl.classList.replace('show', 'hidden');
+}
+
+/**
+ * 
+ * Set specific Rak by button el and content el to be active.
+ * 
+ * @param {obj} btnEl Button to be set active
+ * @param {obj} contentEl Element to be set active
+ */
+const setRakActive = (btnEl, contentEl) => {
+  btnEl.classList.add('toggle__btn__active')
+  contentEl.classList.replace('hidden', 'show');
+}
+
+/**
+ * 
+ * Add event listener to all Rak toggle btn
+ * 
+ */
+allToggleBtn.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const idBtn = btn.id
+
+    // nyalain btn yg diklik, matiin btn lainnya
+    if (btn.className.includes('toggle__btn__active')) {
+      if (idBtn === 'left-content-btn') {
+        setRakNotActive(leftContentBtnEl, leftContentEl)
+      } else if (idBtn === 'right-content-btn') {
+        setRakNotActive(rightContentBtnEl, rightContentEl)
+      }
+    } else {
+      if (idBtn === 'left-content-btn') {
+        setRakActive(leftContentBtnEl, leftContentEl)
+        setRakNotActive(rightContentBtnEl, rightContentEl)
+      } else if (idBtn === 'right-content-btn') {
+        setRakActive(rightContentBtnEl, rightContentEl)
+        setRakNotActive(leftContentBtnEl, leftContentEl)
+      }
+    }
+  })
+})
--- assets/js/state.js
+++ assets/js/state.js
@@ -1,19 +1,13 @@
 let arr = [];
 let idx = 0;
-let stateIdx = 0;
-
-// setState func for updating the specific state value
-const setState = (setStateValue) => {
-  arr[stateIdx] = setStateValue
-
-  useState(setStateValue)
-  console.log('setState', setStateValue);
-}
 
 /**
  * 
  * Inspired by React Hooks useState. This is the lite version
- * abd just experimenting for Dicoding submission.
+ * just experimenting for Dicoding submission. Bcs w're not
+ * rerendering every component in every changes, we still need to
+ * change via it's element.
+ * TODO: Update this hooks for better approach later.
  * 
  * @param {any} initialState The initial value when calling useState
  * @returns An array of state value and the setState function for updating the state
@@ -22,9 +16,15 @@ const setState = (setStateValue) => {
 export const useState = (initialState) => {
   
   // set current index for specific state
-  stateIdx = idx
+  const stateIdx = idx
   // set state
   const state = arr.length === 0 ? initialState : arr[stateIdx];
+
+  // setState func for updating the specific state value
+  const setState = (setStateValue) => {
+    arr[stateIdx] = setStateValue
+    console.log('masuk', setStateValue, arr[stateIdx]);
+  }
 
   // increment idx for the next useState index
   idx++;
