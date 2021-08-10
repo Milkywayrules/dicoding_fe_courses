803f87f208b74fe969ab6043b28357f6f68d46fe
Dio Ilham <apajabole1@gmail.com>
2021-08-10 09:57:34 +0700

    #3: Add NO_SECRET_KEY to const

--- src/const.js
+++ src/const.js
@@ -35,6 +35,8 @@ export const APP_META_DATA = {
 };
 
 export const UTILS = {
+  NOT_SECRET_KEY:
+    '09f26e402586e2faa8da4c98a35f1b20d6b033c6097befa8be3486a829587fe2f90a832bd3ff9d42710a4da095a2ce285b009f0c3730cd9b8e1af3eb84df6611',
   DB_NAME: 'animefinder_dicoding_dio',
   DEFAULT_SORT: 'DESCENDING',
   DEFAULT_SORT_BY: 'id',
